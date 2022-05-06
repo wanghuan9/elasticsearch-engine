6efb1d11b0b969b1b17db5f0e44fce93e482b58e
wanghaun <huan2.wang@aihuishou.com>
2022-05-06 20:31:12 +0800

    feature/proxy proxy 添加异常场景测试示例

---
+++ src/test/java/com/elasticsearch/engine/elasticsearchengine/error/repository/SupplierItemErrorRepository.java
@@ -0,0 +1,20 @@
+package com.elasticsearch.engine.elasticsearchengine.error.repository;
+
+import com.elasticsearch.engine.elasticsearchengine.execute.querymodel.SupplierItem;
+import com.elasticsearch.engine.elasticsearchengine.execute.resultmodel.SupplierItemEntity;
+import com.elasticsearch.engine.elasticsearchengine.model.annotion.EsQueryIndex;
+
+/**
+ * 异常场景测试  未继承BaseESRepository
+ */
+@EsQueryIndex(index = "supplier_item_spare")
+public interface SupplierItemErrorRepository {
+
+    /**
+     * 查询单个
+     *
+     * @param param
+     * @return
+     */
+    SupplierItemEntity queryOne(SupplierItem param);
+}
---
+++ src/test/java/com/elasticsearch/engine/elasticsearchengine/error/repository/SupplierItemParamErrorRepository.java
@@ -0,0 +1,29 @@
+package com.elasticsearch.engine.elasticsearchengine.error.repository;
+
+import com.elasticsearch.engine.elasticsearchengine.execute.querymodel.SupplierItem;
+import com.elasticsearch.engine.elasticsearchengine.execute.resultmodel.SupplierItemEntity;
+import com.elasticsearch.engine.elasticsearchengine.model.annotion.EsQueryIndex;
+import com.elasticsearch.engine.elasticsearchengine.model.domain.BaseESRepository;
+
+/**
+ * 异常场景测试  出入参不符合标准
+ */
+@EsQueryIndex(index = "supplier_item_spare")
+public interface SupplierItemParamErrorRepository extends BaseESRepository<SupplierItemEntity, String> {
+
+    /**
+     * 入参异常测试
+     *
+     * @param param
+     * @return
+     */
+    SupplierItemEntity queryOne(SupplierItem param, SupplierItemEntity item);
+
+    /**
+     * 出参异常测试
+     *
+     * @param param
+     * @return
+     */
+    SupplierItem queryOne(SupplierItem param);
+}

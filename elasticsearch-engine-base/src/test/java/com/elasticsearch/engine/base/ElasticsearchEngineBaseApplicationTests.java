4e53bf762525191ce107bf8b0763634e0520779d
wanghaun <huan2.wang@aihuishou.com>
2022-06-17 21:05:30 +0800

    feature/module_split 模块拆分,解除依赖

---
+++ elasticsearch-engine-base/src/test/java/com/elasticsearch/engine/base/ElasticsearchEngineBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.engine.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchEngineBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ elasticsearch-engine-jpa/src/test/java/com/elasticsearch/engine/jpa/ElasticsearchEngineJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.engine.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchEngineJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

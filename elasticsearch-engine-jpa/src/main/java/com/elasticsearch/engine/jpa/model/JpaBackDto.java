7076b4bd08ceec8f1563a6656413dd56d7f47fef
wanghaun <huan2.wang@aihuishou.com>
2022-07-08 17:13:02 +0800

    feature/20220629_fix 优化

--- elasticsearch-engine-jpa/src/main/java/com/elasticsearch/engine/jpa/model/JpaBackDto.java
+++ elasticsearch-engine-jpa/src/main/java/com/elasticsearch/engine/jpa/model/JpaBackDto.java
@@ -18,7 +18,7 @@ public class JpaBackDto extends BackDto {
     public static BackDto hasJpaBack(Method method) {
         JpaEsQuery esQuery = method.getAnnotation(JpaEsQuery.class);
         String backColumn = esQuery.backColumn();
-        String tableName = esQuery.tableName();
+        String tableName = esQuery.backTable();
         Class<?> backColumnTyp = esQuery.backColumnType();
         if (StringUtils.isNotEmpty(backColumn) && Objects.nonNull(backColumnTyp) && !backColumnTyp.equals(Objects.class)) {
             return BackDto.builder().tableName(tableName).backColumn(backColumn).backColumnTyp(backColumnTyp).build();
